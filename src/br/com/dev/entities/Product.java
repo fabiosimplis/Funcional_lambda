77be9a0368b58b5a6e19bd9d01d5558ba18ec081
fabiosimplis <fabio.simplis@gmail.com>
2024-02-15 19:02:06 -0300

    Function Reference method com método não estático

--- src/br/com/dev/entities/Product.java
+++ src/br/com/dev/entities/Product.java
@@ -48,6 +48,9 @@ public class Product {
         return p.getName().toUpperCase();
     }
 
+    public String nonStaticUpperCaseName(){
+        return  name.toUpperCase();
+    }
     @Override
     public String toString() {
         return "Product " +
