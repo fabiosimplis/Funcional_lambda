8edd875c22233be84cc6c0ad7d4d713a31504306
fabiosimplis <fabio.simplis@gmail.com>
2024-02-15 15:48:32 -0300

    Implementação da interface

---
+++ src/br/com/dev/application/PredicateProgram.java
@@ -0,0 +1,27 @@
+package br.com.dev.application;
+
+import br.com.dev.entities.Product;
+import br.com.dev.util.ProductPredicate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PredicateProgram {
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.00));
+        list.add(new Product("HD Case", 80.00));
+
+        //Remover todos que tenham preço acima de 100
+        //list.removeIf(p -> p.getPrice() >= 100);
+
+        list.removeIf(new ProductPredicate());
+
+        for(Product p: list){
+            System.out.println(p);
+        }
+    }
+}
---
+++ src/br/com/dev/util/ProductPredicate.java
@@ -0,0 +1,14 @@
+package br.com.dev.util;
+
+import br.com.dev.entities.Product;
+
+import java.util.function.Predicate;
+
+public class ProductPredicate implements Predicate<Product> {
+
+    @Override
+    public boolean test(Product p) {
+
+        return p.getPrice() >= 100.0;
+    }
+}
